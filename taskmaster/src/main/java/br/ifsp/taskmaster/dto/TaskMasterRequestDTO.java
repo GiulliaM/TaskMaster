8bcbd9b3c669fff1a6d485ae9571de750fa9d501
GiulliaM <giulliameneses@gmail.com>
2026-05-17 00:17:00 -0300

    feat: novas classes dtos

---
+++ taskmaster/src/main/java/br/ifsp/taskmaster/dto/TaskMasterRequestDTO.java
@@ -0,0 +1,62 @@
+package br.ifsp.taskmaster.dto;
+
+import jakarta.validation.constraints.*;
+
+import java.time.LocalDate;
+
+public class TaskMasterRequestDTO {
+
+    @NotBlank(message = "O titulo deve ser obrigatório")
+    private String titulo;
+    @NotBlank(message = "A descrição deve ser obrigatória")
+    private String descricao;
+    @NotBlank(message = "O titulo deve ser obrigatório")
+    private String categoria;
+    @NotBlank(message = "A data limite deve ser obrigatória")
+    @FutureOrPresent(message = "A data limte não deve ser no passado")
+    private LocalDate dataLimite;
+    @NotNull(message = "A prioridade deve ser obrigatória")
+    @Min(value = 1, message = "Prioridade mínima é 1")
+    @Max(value = 5, message = "Prioridade máxima é 5")
+    private Integer prioridade;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+
+    public LocalDate getDataLimite() {
+        return dataLimite;
+    }
+
+    public void setDataLimite(LocalDate dataLimite) {
+        this.dataLimite = dataLimite;
+    }
+
+    public Integer getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Integer prioridade) {
+        this.prioridade = prioridade;
+    }
+}
---
+++ taskmaster/src/main/java/br/ifsp/taskmaster/dto/TaskMasterResponseDTO.java
@@ -0,0 +1,61 @@
+package br.ifsp.taskmaster.dto;
+
+import java.time.LocalDate;
+
+public class TaskMasterResponseDTO {
+
+    private Long id;
+    private String titulo;
+    private String descricao;
+    private String categoriaa;
+    private LocalDate dataLimite;
+    private Integer prioridade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getCategoriaa() {
+        return categoriaa;
+    }
+
+    public void setCategoriaa(String categoriaa) {
+        this.categoriaa = categoriaa;
+    }
+
+    public LocalDate getDataLimite() {
+        return dataLimite;
+    }
+
+    public void setDataLimite(LocalDate dataLimite) {
+        this.dataLimite = dataLimite;
+    }
+
+    public Integer getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(Integer prioridade) {
+        this.prioridade = prioridade;
+    }
+}
